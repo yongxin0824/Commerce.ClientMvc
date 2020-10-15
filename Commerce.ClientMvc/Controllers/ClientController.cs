875faf7578922adcf08bf3174d2b8910f00bcebb
‘““longen0707” <907417943@qq.com>
2020-10-15 08:40:39 +0800

    添加项目文件。

---
+++ Commerce.ClientMvc/Controllers/ClientController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commerce.ClientMvc.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult login()
+        {
+            return View();
+        }
+        //物流方案页面
+        public IActionResult wuliu()
+        {
+            return View();
+        }
+        //注册页面
+        public IActionResult reg()
+        {
+            return View();
+        }
+        //一级购物车页面
+        public IActionResult cart()
+        {
+            return View();
+        }
+        //确定订单信息
+        public IActionResult cart2()
+        {
+            return View();
+        }
+        //完成支付页面
+        public IActionResult cart3()
+        {
+            return View();
+        }
+    }
+}
---
+++ Commerce.ClientMvc/Controllers/merchantController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commerce.ClientMvc.Controllers
+{
+    public class merchantController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
